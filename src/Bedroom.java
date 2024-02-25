67efc136fcc5db07c1cf993b6e2f2196bdd87ab8
adamd <adam.d.hinton@gmail.com>
2024-02-24 20:53:09 -0500

    first draft solution

--- src/Bedroom.java
+++ src/Bedroom.java
@@ -1,5 +1,34 @@
 public class Bedroom {
 
+    private String name;
+    private Wall wall1;
+    private Wall wall2;
+    private Wall wall3;
+    private Wall wall4;
+    private Ceiling ceiling;
+    private Bed bed;
+    private Lamp lamp;
+
+    public Bedroom(String name, Wall wall1, Wall wall2, Wall wall3, Wall wall4, Ceiling ceiling, Bed bed, Lamp lamp) {
+        this.name = name;
+        this.wall1 = wall1;
+        this.wall2 = wall2;
+        this.wall3 = wall3;
+        this.wall4 = wall4;
+        this.ceiling = ceiling;
+        this.bed = bed;
+        this.lamp = lamp;
+    }
+
+    public Lamp getLamp() {
+        return lamp;
+    }
+
+    public void makeBed(){
+        System.out.print("Bedroom -> Making bed | ");
+        bed.make();
+    }
+
 
 }
 
@@ -71,8 +100,7 @@ class Bed{
     }
 
     public void make(){
-        System.out.print("Bedroom -> Making bed | ");
-    }
+        System.out.print("Bed -> Making | ");    }
 }
 
 class Ceiling{
@@ -100,4 +128,5 @@ class Wall{
         this.direction = direction;
     }
 }
+
 
